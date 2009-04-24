27959126a5e0a8d3e1f7df1f1e04178794403898
Christian KAKESA <christian.kakesa@gmail.com>
2009-04-24 12:43:40 -0700

    Initial commit

---
+++ trunk/test/osd.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+#include "../src/WinOSD.h"
+
+int WINAPI WinMain(::HINSTANCE FirstInstance, ::HINSTANCE PrevInstance, ::LPSTR CmdLine, int CmdShow)
+{
+    (void)FirstInstance; //Just for ansi compliance => unused variable warning
+    (void)PrevInstance; //Just for ansi compliance => unused variable warning
+    (void)CmdLine; //Just for ansi compliance => unused variable warning
+    (void)CmdShow; //Just for ansi compliance => unused variable warning
+
+    WinOSD* wosd = new WinOSD(); //FirstInstance);
+    wosd->PrintText();
+    //wosd->PrintText(static_cast<Glib::ustring>("TOTO !!!"));
+    return (EXIT_SUCCESS);
+}
