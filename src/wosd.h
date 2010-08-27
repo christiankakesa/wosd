6c70a9f2531b8fe7ec84e2bee4ff108cc65a534f
Christian Kakesa <christian.kakesa@gmail.com>
2010-08-27 23:26:07 +0200

    Add wosd.h as global header

---
+++ src/wosd.h
@@ -0,0 +1,9 @@
+// wosd.h
+// Christian KAKESA <christian.kakesa@gmail.com>
+#ifndef __WOSD_H_
+#define __WOSD_H_
+
+#define AUTHOR_NAME		"Christian Kakesa <christian.kakesa@gmail.com>"
+#define AUTHOR_WEBSITE	"http://christian.kakesa.net"
+
+#endif /* !__WOSD_H_ */
